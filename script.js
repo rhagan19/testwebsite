bf9b4005fa4fc5d3f6ae30802e1b66af088a092d
rhagan19 <73906621+rhagan19@users.noreply.github.com>
2024-03-01 23:49:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ function openEmail() {
   // Construct the mailto link with subject and body
   window.location.href = `mailto:info@magnvs.io?subject=${subject}&body=${emailBody}`;
 }
+
+// Event listener for the "Curriculums" button/link
+document.addEventListener('DOMContentLoaded', function() {
+  var curriculumsLink = document.querySelector('.curriculums-link');
+  if (curriculumsLink) {
+    curriculumsLink.addEventListener('click', function() {
+      window.location.href = 'curriculums.html'; // Link to your curriculums page
+    });
+  }
+
+  // Event listener for the "Updates" button/link
+  var updatesLink = document.querySelector('.updates-link');
+  if (updatesLink) {
+    updatesLink.addEventListener('click', function() {
+      window.location.href = 'updates.html'; // Link to your updates page
+    });
+  }
+});
